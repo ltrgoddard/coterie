7025cc23d46993230b3bf23370de3375ae6df978
ltrgoddard <louisgoddard@gmail.com>
2016-03-14 13:14:47 +0000

    example PNG and deletion of singletons

--- wiki.R
+++ wiki.R
@@ -3,9 +3,10 @@ library(igraph)
 args <- commandArgs(trailingOnly = TRUE) 
 people <- as.matrix(read.csv(args[1])) # input file - first command line argument
 people.g <- graph.adjacency(people, mode="undirected", weighted=TRUE, diag=FALSE)
+people.g <- delete.vertices(people.g,which(degree(people.g)<1))
 la <- layout.fruchterman.reingold(people.g, niter=500, weights=E(people.g)$weight)
 
-png(file=args[2], width=1500, height=1500) # output file - second command line argument
-plot(people.g, layout=la, vertex.shape="circle", vertex.size=1, edge.width=(E(people.g)$weight)/2)
+png(file=args[2], width=1000, height=1000) # output file - second command line argument
+plot(people.g, layout=la, vertex.shape="circle", vertex.size=1, edge.width=1)
 
 dev.off()
